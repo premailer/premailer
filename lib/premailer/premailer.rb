749542395eeb98af110872681531f66b969b35ea
Alex Dunae <alex@dunae.ca>
2009-12-09 14:15:42 -0800

    version bump

--- lib/premailer/premailer.rb
+++ lib/premailer/premailer.rb
@@ -33,7 +33,7 @@ class Premailer
   include HtmlToPlainText
   include CssParser
 
-  VERSION = '1.5.3'
+  VERSION = '1.5.4'
 
   CLIENT_SUPPORT_FILE = File.dirname(__FILE__) + '/../../misc/client_support.yaml'
 
