d8c34b4a624a0c052e48400f12be5d5ce62acb2e
Alex Dunae <alex@dunae.ca>
2010-11-04 13:19:51 -0700

    Test case for self-closing XHTML tags

--- test/test_premailer.rb
+++ test/test_premailer.rb
@@ -4,10 +4,24 @@ require File.dirname(__FILE__) + '/helper'
 class TestPremailer < Test::Unit::TestCase
   include WEBrick
 
-  def atest_accents
+  def test_accents
     local_setup
 
     assert_equal 'cédille cé &amp; garçon garçon à à', @doc.at('#specialchars').inner_html
+  end
+  
+  def test_self_closing_xhtml
+    html = <<END_HTML
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    <html>
+    <body>
+    <br/>
+    </body>
+		</html>
+END_HTML
+
+		premailer = Premailer.new(html, :with_html_string => true)
+    assert_match /<br[\s]*\/>/, premailer.to_s
   end
   
   def test_preserving_html_entities
@@ -17,7 +31,7 @@ class TestPremailer < Test::Unit::TestCase
 		assert_match /<p>&nbsp; &amp;<\/p>/, premailer.to_s
   end
 
-  def atest_link_query_string
+  def test_link_query_string
     qs = 'utm_source=1234&tracking=good&amp;doublescape'
     remote_setup(:link_query_string => qs)
     
@@ -29,7 +43,7 @@ class TestPremailer < Test::Unit::TestCase
     end
   end
   
-  def atest_mailtos_with_query_strings
+  def test_mailtos_with_query_strings
     html = <<END_HTML
     <html>
 		<a href="mailto:info@example.com?subject=Programmübersicht&amp;body=Lorem ipsum dolor sit amet.">Test</a>
@@ -43,14 +57,14 @@ END_HTML
 	  assert_no_match /testing=123/, premailer.processed_doc.search('a').first.attributes['href'].to_s    
   end
   
-  def atest_escaping_strings
+  def test_escaping_strings
     local_setup
   
     str = %q{url("/images/test.png");}
     assert_equal("url(\'/images/test.png\');", Premailer.escape_string(str))
   end
   
-  def atest_importing_local_css
+  def test_importing_local_css
     local_setup
 
     # noimport.css (print stylesheet) sets body { background } to red
@@ -60,7 +74,7 @@ END_HTML
     assert_match /display: none/, @doc.at('#hide01').attributes['style'].to_s
   end
 
-  def atest_importing_remote_css
+  def test_importing_remote_css
     remote_setup
   
     # noimport.css (print stylesheet) sets body { background } to red
@@ -70,7 +84,7 @@ END_HTML
     assert_match /display: none/, @doc.at('#hide01').attributes['style']
   end
 
-  def atest_local_remote_check
+  def test_local_remote_check
     assert Premailer.local_data?( StringIO.new('a') )
     assert Premailer.local_data?( '/path/' )
     assert !Premailer.local_data?( 'http://example.com/path/' )
@@ -80,13 +94,13 @@ END_HTML
     assert premailer.local_uri?( '/path/' )
   end
   
-  def atest_initialize_can_accept_io_object
+  def test_initialize_can_accept_io_object
     io = StringIO.new('hi mom')
     premailer = Premailer.new(io)
     assert_match premailer.to_inline_css, /hi mom/
   end
   
-  def atest_initialize_can_accept_html_string
+  def test_initialize_can_accept_html_string
     premailer = Premailer.new('<p>test</p>', :with_html_string => true)
     assert_match premailer.to_inline_css, /test/
   end
