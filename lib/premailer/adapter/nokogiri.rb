premailer/premailer

commit dfed0b44ed32c4ac53dca92213a2a1e3793a917a

Support for Nokogiri Fragment in all adapters (#350)
based on the work in https://github.com/premailer/premailer/pull/348

lib/premailer/adapter/nokogiri.rb
@@ -222,16 +222,16 @@ class Premailer
         end
         # Default encoding is ASCII-8BIT (binary) per http://groups.google.com/group/nokogiri-talk/msg/0b81ef0dc180dc74
         # However, we really don't want to hardcode this. ASCII-8BIT should be the default, but not the only option.
-        if thing.is_a?(String) and RUBY_VERSION =~ /1.9/
+        encoding = if thing.is_a?(String) and RUBY_VERSION =~ /1.9/
           thing = thing.force_encoding(@options[:input_encoding]).encode!
-          doc = ::Nokogiri::HTML(thing, nil, @options[:input_encoding]) { |c| c.recover }
+          @options[:input_encoding]
         else
-          default_encoding = RUBY_PLATFORM == 'java' ? nil : 'BINARY'
-          doc = if @options[:html_fragment]
-            ::Nokogiri::HTML.fragment(thing, @options[:input_encoding] || default_encoding)
-          else
-            ::Nokogiri::HTML(thing, nil, @options[:input_encoding] || default_encoding) { |c| c.recover }
-          end
+          @options[:input_encoding] || RUBY_PLATFORM == 'java' ? nil : 'BINARY'
+        end
+        doc = if @options[:html_fragment]
+          ::Nokogiri::HTML.fragment(thing, encoding)
+        else
+          ::Nokogiri::HTML(thing, nil, encoding) { |c| c.recover }
         end
 
         # Fix for removing any CDATA tags from both style and script tags inserted per

lib/premailer/adapter/nokogiri_fast.rb
@@ -150,12 +150,19 @@ class Premailer
       # @return [::Nokogiri::XML] a document.
       def write_unmergable_css_rules(doc, unmergable_rules) # :nodoc:
         styles = unmergable_rules.to_s
-        return doc  if styles.empty?
-        style_tag = doc.create_element "style", styles
-        head = doc.at_css('head')
-        head ||=  doc.root.first_element_child.add_previous_sibling(doc.create_element "head")  if doc.root && doc.root.first_element_child
-        head ||=  doc.add_child(doc.create_element "head")
-        head << style_tag
+        unless styles.empty?
+          if @options[:html_fragment]
+            style_tag = ::Nokogiri::XML::Node.new("style", doc)
+            style_tag.content = styles
+            doc.add_child(style_tag)
+          else
+            style_tag = doc.create_element "style", styles
+            head = doc.at_css('head')
+            head ||= doc.root.first_element_child.add_previous_sibling(doc.create_element "head") if doc.root && doc.root.first_element_child
+            head ||= doc.add_child(doc.create_element "head")
+            head << style_tag
+          end
+        end
         doc
       end
 
@@ -217,12 +224,16 @@ class Premailer
         end
         # Default encoding is ASCII-8BIT (binary) per http://groups.google.com/group/nokogiri-talk/msg/0b81ef0dc180dc74
         # However, we really don't want to hardcode this. ASCII-8BIT should be the default, but not the only option.
-        if thing.is_a?(String) and RUBY_VERSION =~ /1.9/
+        encoding = if thing.is_a?(String) and RUBY_VERSION =~ /1.9/
           thing = thing.force_encoding(@options[:input_encoding]).encode!
-          doc = ::Nokogiri::HTML(thing, nil, @options[:input_encoding]) { |c| c.recover }
+          @options[:input_encoding]
         else
-          default_encoding = RUBY_PLATFORM == 'java' ? nil : 'BINARY'
-          doc = ::Nokogiri::HTML(thing, nil, @options[:input_encoding] || default_encoding) { |c| c.recover }
+          @options[:input_encoding] || RUBY_PLATFORM == 'java' ? nil : 'BINARY'
+        end
+        doc = if @options[:html_fragment]
+          ::Nokogiri::HTML.fragment(thing, encoding)
+        else
+          ::Nokogiri::HTML(thing, nil, encoding) { |c| c.recover }
         end
 
         # Fix for removing any CDATA tags from both style and script tags inserted per

lib/premailer/adapter/nokogumbo.rb
@@ -148,12 +148,19 @@ class Premailer
       # @return [::Nokogiri::XML] a document.
       def write_unmergable_css_rules(doc, unmergable_rules) # :nodoc:
         styles = unmergable_rules.to_s
-        return doc  if styles.empty?
-        style_tag = doc.create_element "style", styles
-        head = doc.at_css('head')
-        head ||=  doc.root.first_element_child.add_previous_sibling(doc.create_element "head")  if doc.root && doc.root.first_element_child
-        head ||=  doc.add_child(doc.create_element "head")
-        head << style_tag
+        unless styles.empty?
+          if @options[:html_fragment]
+            style_tag = ::Nokogiri::XML::Node.new("style", doc)
+            style_tag.content = styles
+            doc.add_child(style_tag)
+          else
+            style_tag = doc.create_element "style", styles
+            head = doc.at_css('head')
+            head ||= doc.root.first_element_child.add_previous_sibling(doc.create_element "head") if doc.root && doc.root.first_element_child
+            head ||= doc.add_child(doc.create_element "head")
+            head << style_tag
+          end
+        end
         doc
       end
 
@@ -217,10 +224,11 @@ class Premailer
         # However, we really don't want to hardcode this. ASCII-8BIT should be the default, but not the only option.
         if thing.is_a?(String) and RUBY_VERSION =~ /1.9/
           thing = thing.force_encoding(@options[:input_encoding]).encode!
-          doc = ::Nokogiri::HTML5(thing)
+        end
+        doc = if @options[:html_fragment]
+          ::Nokogiri::HTML5(thing)
         else
-          default_encoding = RUBY_PLATFORM == 'java' ? nil : 'BINARY'
-          doc = ::Nokogiri::HTML5(thing)
+          ::Nokogiri::HTML5.fragment(thing)
         end
 
         # Fix for removing any CDATA tags from both style and script tags inserted per

commit d68e457924876cd806ca362571c27d32b49966a6

Fix input encoding in nokogiri adapters
The bug was introduced in #350 and it's related to operator precedence

lib/premailer/adapter/nokogiri.rb
@@ -226,7 +226,7 @@ class Premailer
           thing = thing.force_encoding(@options[:input_encoding]).encode!
           @options[:input_encoding]
         else
-          @options[:input_encoding] || RUBY_PLATFORM == 'java' ? nil : 'BINARY'
+          @options[:input_encoding] || (RUBY_PLATFORM == 'java' ? nil : 'BINARY')
         end
         doc = if @options[:html_fragment]
           ::Nokogiri::HTML.fragment(thing, encoding)

lib/premailer/adapter/nokogiri_fast.rb
@@ -228,7 +228,7 @@ class Premailer
           thing = thing.force_encoding(@options[:input_encoding]).encode!
           @options[:input_encoding]
         else
-          @options[:input_encoding] || RUBY_PLATFORM == 'java' ? nil : 'BINARY'
+          @options[:input_encoding] || (RUBY_PLATFORM == 'java' ? nil : 'BINARY')
         end
         doc = if @options[:html_fragment]
           ::Nokogiri::HTML.fragment(thing, encoding)