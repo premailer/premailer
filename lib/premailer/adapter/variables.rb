00e8b5eabd799ef44a5a382d4bafde3bdeba6d19
Flip Sasser <flip.sasser@gmail.com>
2025-01-03 17:27:59 -0800

    rubocop stuff

--- lib/premailer/adapter/variables.rb
+++ lib/premailer/adapter/variables.rb
@@ -1,14 +1,18 @@
+# frozen_string_literal: true
+
 module AdapterHelper
   module Variables
     def map_variables(declaration)
-      while declaration.match?(/var\(\s*--([^)]+)\s*\)/)
+      while declaration.match?(/\bvar\(\s*--([^)]+)\s*\)/)
         final_declarations = []
         declarations = declaration.split(";").map(&:strip).reject(&:empty?)
         declarations.each do |declaration|
-          if match = declaration.match(/\bvar\(\s*--([^)]+)\s*\)/)
+          match = declaration.match(/\bvar\(\s*--([^)]+)\s*\)/)
+          if match
             variable = match[0]
             variable_name = match[1]
-            if variable_value = css_variables[variable_name]
+            variable_value = css_variables[variable_name]
+            unless variable_value.empty?
               final_declarations.push(declaration.gsub(variable, variable_value))
             end
           else
@@ -24,15 +28,15 @@ module AdapterHelper
     private
 
     def css_variables
-      @css_variables ||= @css_parser.find_by_selector(":root").reduce({}) do |memo, rule|
-        rules = rule.split(";")
+      @css_variables ||= @css_parser.find_by_selector(":root").each_with_object({}) do |ruleset, memo|
+        rules = ruleset.split(";")
         rules.each do |rule|
           rule = rule.strip
-          if match = rule.match(/--([^:]+):(.+)/)
+          match = rule.match(/--([^:]+):(.+)/)
+          if match
             memo[match[1].strip] = match[2].strip
           end
         end
-        memo
       end
     end
   end
