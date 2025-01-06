dbc97c0b873ff97486633a5a304ef77c730b2e4a
Flip Sasser <flip.sasser@gmail.com>
2025-01-06 14:53:01 -0800

    that's a little better

--- test/test_css_variables.rb
+++ test/test_css_variables.rb
@@ -2,26 +2,27 @@
 require __dir__ + '/helper'
 
 class TestCssVariables < Premailer::TestCase
-  [:nokogiri, :nokogumbo, :nokogiri_fast].each do |adapter|
-    class_eval <<~RUBY, __FILE__, __LINE__ + 1
-      # Test that CSS variables are inlined correctly with the #{adapter} adapter
-      def test_css_variables_in_#{adapter}
-        html = <<~END_HTML
-          <html>
-          <body>
-          <style type="text/css"> :root { --red: #f00; --yellow: #ff0; } </style>
-          <style type="text/css"> p { color: var(--red); border: 1px solid var(--yellow); } </style>
-          <p>Test</p>
-          </body>
-          </html>
-        END_HTML
+  def test_css_variables
+    html = <<~END_HTML
+      <html>
+      <body>
+      <style type="text/css"> :root { --red: #f00; --yellow: #ff0; } </style>
+      <style type="text/css"> p { color: var(--red); border: 1px solid var(--yellow); } </style>
+      <p>Test</p>
+      </body>
+      </html>
+    END_HTML
 
-        premailer = Premailer.new(html, :adapter => :#{adapter}, :with_html_string => true)
-        premailer.to_inline_css
+    color_regex = /color:\s*#f00/i
+    border_regex = /border:\s*1px solid #ff0/i
 
-        assert_match /color:\s*#f00/i, premailer.processed_doc.at('p')['style']
-        assert_match /border:\s*1px solid #ff0/i, premailer.processed_doc.at('p')['style']
-      end
-    RUBY
+    [:nokogiri, :nokogumbo, :nokogiri_fast].each do |adapter|
+      premailer = Premailer.new(html, :adapter => adapter, :with_html_string => true)
+      premailer.to_inline_css
+      styles = premailer.processed_doc.at('p')['style']
+
+      assert_match(color_regex, styles, "Using adapter :#{adapter}")
+      assert_match(border_regex, styles, "Using adapter :#{adapter}")
+    end
   end
 end
