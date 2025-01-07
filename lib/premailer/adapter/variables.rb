26d11a6f755ed5a2370f483af1a7b61efeafae8e
Flip Sasser <flip.sasser@gmail.com>
2025-01-07 15:31:04 -0800

    test and fix some misses, and PR feedback

--- lib/premailer/adapter/variables.rb
+++ lib/premailer/adapter/variables.rb
@@ -2,27 +2,27 @@
 
 module AdapterHelper
   module Variables
-    def map_variables(declaration)
-      while declaration.match?(/\bvar\(\s*--([^)]+)\s*\)/)
-        final_declarations = []
-        declarations = declaration.split(";").map(&:strip).reject(&:empty?)
-        declarations.each do |declaration|
-          match = declaration.match(/\bvar\(\s*--([^)]+)\s*\)/)
-          if match
-            variable = match[0]
-            variable_name = match[1]
-            variable_value = css_variables[variable_name]
-            unless variable_value.empty?
-              final_declarations.push(declaration.gsub(variable, variable_value))
-            end
-          else
-            final_declarations.push(declaration)
-          end
+    CSS_VARIABLE_REGEX = /\bvar\(\s*--([^\s)]+)\s*\)/
+    # Takes a CSS declaration string and replaces any `var(--my-variable-name)` references with
+    # variables loaded from document's styles
+    def map_variables(styles)
+      return styles if css_variables.empty?
+      return styles unless styles.match?(CSS_VARIABLE_REGEX)
+
+      final_declarations = []
+      declarations = styles.split(";").map(&:strip).reject(&:empty?)
+      declarations.each do |declaration|
+        while match = declaration.match(CSS_VARIABLE_REGEX)
+          variable = match[0]
+          variable_name = match[1].downcase
+          variable_value = css_variables[variable_name]
+          declaration = declaration.gsub(variable, variable_value)
         end
-        declaration = final_declarations.join(";")
+
+        final_declarations.push(declaration)
       end
 
-      declaration
+      final_declarations.join(";")
     end
 
     private
@@ -31,8 +31,8 @@ module AdapterHelper
       @css_variables ||= @css_parser.find_by_selector(":root").each_with_object({}) do |ruleset, memo|
         rules = ruleset.split(";")
         rules.each do |rule|
-          rule = rule.strip
-          match = rule.match(/--([^:]+):(.+)/)
+          rule.strip!
+          match = rule.match(/--([^:\s]+):(.+)/)
           if match
             memo[match[1].strip] = match[2].strip
           end
--- test/test_css_variables.rb
+++ test/test_css_variables.rb
@@ -6,23 +6,51 @@ class TestCssVariables < Premailer::TestCase
     html = <<~END_HTML
       <html>
       <body>
-      <style type="text/css"> :root { --red: #f00; --yellow: #ff0; } </style>
-      <style type="text/css"> p { color: var(--red); border: 1px solid var(--yellow); } </style>
-      <p>Test</p>
+      <style type="text/css"> :root { --red: #f00; --yellow: #ff0; --a-complicated-variableName: url("/images/logo.png"); } </style>
+      <style type="text/css">
+        .container { background-color: orange; }
+        .container p { color: var(  --red )  ; border: 1px solid var(--yellow); background-image: linear-gradient(from top, var( --red ), var( --yellow )); }
+        /* I realize this isn't the correct shorthand order for background, but I'd like to test
+         * variables that are nested within the rest of the declaration */
+        .brand { background: no-repeat var(--a-complicated-variableName) center center; }
+      </style>
+      <div class="container">
+        <div class="brand"></div>
+        <p>Test</p>
+      </div>
       </body>
       </html>
     END_HTML
 
-    color_regex = /color:\s*#f00/i
-    border_regex = /border:\s*1px solid #ff0/i
+    # This should be unaffected by CSS variable parsing
+    container_content = "background-color: orange"
+
+    # These should be replaced directly
+    p_color_content = "color: #f00"
+    p_border_content = "border: 1px solid #ff0"
+    p_gradient_content = "linear-gradient(from top, #f00, #ff0)"
+
+    # The CSS parser will heplfully correct the shorthand order for background, but we still want to
+    # ensure the variable is interpolated
+    image_content = 'background: url("/images/logo.png") no-repeat center center'
 
     [:nokogiri, :nokogumbo, :nokogiri_fast].each do |adapter|
       premailer = Premailer.new(html, :adapter => adapter, :with_html_string => true)
       premailer.to_inline_css
-      styles = premailer.processed_doc.at('p')['style']
+      container_styles = premailer.processed_doc.at('.container')['style']
+      p_styles = premailer.processed_doc.at('p')['style']
+      brand_styles = premailer.processed_doc.at('.brand')['style']
 
-      assert_match(color_regex, styles, "Using adapter :#{adapter}")
-      assert_match(border_regex, styles, "Using adapter :#{adapter}")
+      assert(container_styles.include?(container_content),
+             "Expected #{container_styles.inspect} to include #{container_content.inspect} (using adapter :#{adapter})")
+      assert(p_styles.include?(p_color_content),
+             "Expected #{p_styles.inspect} to include #{p_color_content.inspect} (using adapter :#{adapter})")
+      assert(p_styles.include?(p_border_content),
+             "Expected #{p_styles.inspect} to include #{p_border_content.inspect} (using adapter :#{adapter})")
+      assert(p_styles.include?(p_gradient_content),
+             "Expected #{p_styles.inspect} to include #{p_gradient_content.inspect} (using adapter :#{adapter})")
+      assert(brand_styles.include?(image_content),
+             "Expected #{brand_styles.inspect} to include #{image_content.inspect} (using adapter :#{adapter})")
     end
   end
 end
